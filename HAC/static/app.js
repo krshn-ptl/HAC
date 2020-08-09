37eb42c81e946a55cf60f7b8309b73140d86e069
krshn-ptl <58908961+krshn-ptl@users.noreply.github.com>
2020-08-09 11:47:12 +0530

    some edits

--- HAC/static/app.js
+++ HAC/static/app.js
@@ -6,13 +6,13 @@ $(document).ready(function () {
             html_code += '<option> Select ' + id + '</option>';
             $.each(data, function (key, value) {
                 if (id == 'Symptom') {
-                    html_code += '<option>' + value.Symptom + '</option>';
+                    html_code += '<option value="+' + value.Symptom + '">' + value.Symptom + '</option>';
                 }
                 $('#dp1').html(html_code);
                 $('#dp2').html(html_code);
                 $('#dp3').html(html_code);
             })
-        });
+        })
     }
     var url = "https://api.covid19india.org/data.json"
 
